35e5db27f4a15818da26f3f9a8fe37f54ec33c28
m17621679833 <m17621679833@163.com>
2019-07-26 10:49:06 +0800

    fs

--- src/main/java/com/siti/wisdomhydrologic/justforyou/Test.java
+++ src/main/java/com/siti/wisdomhydrologic/justforyou/Test.java
@@ -1,9 +1,11 @@
 package com.siti.wisdomhydrologic.justforyou;
 
-import com.alibaba.fastjson.JSON;
-import io.swagger.annotations.ApiImplicitParam;
+import com.siti.wisdomhydrologic.util.ExceptionUtil;
+import com.siti.wisdomhydrologic.util.enumbean.SystemError;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.http.HttpStatus;
+import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -13,15 +15,18 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 public class Test {
-
-    //用户传过来的模板文件 存储字符串 返回仍旧为字符串
-    @RequestMapping(value = "saveText", method = RequestMethod.POST,
-            produces = {"application/json"})
-    @ResponseStatus(HttpStatus.OK)
-    @ApiOperation(value = "前台上传的模板文件",
-            notes = "前台转换模板成字符串")
-    @ApiImplicitParam(name = "text", value = "前台模板转换之后的字符串", required = true, dataType = "String")
-    public String saveText(@RequestParam("text") String text) {
-        return JSON.toJSONString(text);
+    private static final Logger logger= LoggerFactory.getLogger(Test.class);
+    @RequestMapping(value = "/rest/article", method = RequestMethod.POST, produces = "application/json")
+    @ApiOperation(value = "接口说明",httpMethod = "post",notes = "接口发布说明")
+    @ApiParam(name = "参数",value = "这是描述参数")
+    public String testSwagger2(@RequestBody String name) {
+        return name;
+    }
+    
+    public void quartzJob() {
+        logger.info("----------------------here comes!-------------------------------");
+        logger.error("----------------------here comes!-------------------------------");
+        ExceptionUtil.throwException(SystemError.SENDLIST_NULL);
+        System.out.println("----------------------here comes!-------------------------------");
     }
 }
