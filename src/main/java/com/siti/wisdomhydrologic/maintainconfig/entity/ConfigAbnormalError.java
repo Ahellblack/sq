2eadf7d34a95c3b1c23138d2afca2b85d48f1fef
SITI0410 <1127787221@qq.com>
2019-09-27 21:38:46 +0800

    完成 aberror表的接口

--- src/main/java/com/siti/wisdomhydrologic/maintainconfig/entity/ConfigAbnormalError.java
+++ src/main/java/com/siti/wisdomhydrologic/maintainconfig/entity/ConfigAbnormalError.java
@@ -1,47 +1,51 @@
 package com.siti.wisdomhydrologic.maintainconfig.entity;
 
-/**
- * Created by dell on 2019/9/27.
- */
 public class ConfigAbnormalError {
 
-    private String errorId;
-    private String errorName;
-    private String belongWhichTable;
+  private String errorId;
+  private String errorName;
+  private Integer belongWhichTable;
+  private String table1Relate;
+  private String description;
 
-    public String getErrorId() {
-        return errorId;
-    }
+  public String getErrorId() {
+    return errorId;
+  }
 
-    public void setErrorId(String errorId) {
-        this.errorId = errorId;
-    }
+  public void setErrorId(String errorId) {
+    this.errorId = errorId;
+  }
 
-    public String getErrorName() {
-        return errorName;
-    }
+  public String getErrorName() {
+    return errorName;
+  }
 
-    public void setErrorName(String errorName) {
-        this.errorName = errorName;
-    }
+  public void setErrorName(String errorName) {
+    this.errorName = errorName;
+  }
 
-    public String getBelongWhichTable() {
-        return belongWhichTable;
-    }
+  public Integer getBelongWhichTable() {
+    return belongWhichTable;
+  }
 
-    public void setBelongWhichTable(String belongWhichTable) {
-        this.belongWhichTable = belongWhichTable;
-    }
+  public void setBelongWhichTable(Integer belongWhichTable) {
+    this.belongWhichTable = belongWhichTable;
+  }
 
-    public Integer getTable1Relate() {
-        return table1Relate;
-    }
+  public String getTable1Relate() {
+    return table1Relate;
+  }
 
-    public void setTable1Relate(Integer table1Relate) {
-        this.table1Relate = table1Relate;
-    }
+  public void setTable1Relate(String table1Relate) {
+    this.table1Relate = table1Relate;
+  }
 
-    private Integer table1Relate;
+  public String getDescription() {
+    return description;
+  }
 
+  public void setDescription(String description) {
+    this.description = description;
+  }
 
 }
