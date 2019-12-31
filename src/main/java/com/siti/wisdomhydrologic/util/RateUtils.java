f47a1a88bc5546d2e6cafdd2b3b23867930f458b
Ahellbalck <1229320501@qq.com>
2019-12-31 15:00:43 +0800

    1231

--- src/main/java/com/siti/wisdomhydrologic/util/RateUtils.java
+++ src/main/java/com/siti/wisdomhydrologic/util/RateUtils.java
@@ -28,7 +28,27 @@ public class RateUtils {
         double accuracy_num = num / total ;
         String times = df.format(accuracy_num);
         //数字超过1000会出现蜜汁逗号
-         times = times.replaceAll(",", "");
+        times = times.replaceAll(",", "");
         return Double.parseDouble(times);
     }
+    //修改double字符的精度
+    public static Double setPrecision(double num ,int scale) {
+        DecimalFormat df = (DecimalFormat) NumberFormat.getInstance();
+        //可以设置精确几位小数
+        df.setMaximumFractionDigits(scale);
+        //模式 例如四舍五入
+        df.setRoundingMode(RoundingMode.HALF_UP);
+        String times = df.format(num);
+        //数字超过1000会出现蜜汁逗号
+        times = times.replaceAll(",", "");
+        return Double.parseDouble(times);
+    }
+
+    public static void main(String[] args) {
+
+        String year = "123456";
+        System.out.println(year.substring(0,4));
+
+    }
+
 }
